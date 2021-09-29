e58b109b9d8f7cba598d1448040ae71ccd717c63
raninder <raninderdhillon@gmail.com>
2021-09-29 16:38:04 +0000

    conditions for weather

---
+++ conditionals-2.js
@@ -0,0 +1,10 @@
+const temperature=12;
+if(temperature<0){
+console.log("wear jacket");}
+else if(temperature<15){
+  console.log("wear full sleeves");
+  }
+else{
+  console.log("short sleeves fine");
+}
+  console.log("Now you're ready to go");
