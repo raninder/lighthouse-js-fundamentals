cae1b36e16a376a470330ffdfab638aa82136f45
raninder <raninderdhillon@gmail.com>
2021-10-09 02:46:06 +0000

    looping range

--- looping-range.js
+++ looping-range.js
@@ -1,14 +1,19 @@
-const arr = [];
 const range = function(start, end, step){
-  if((start > end) || (start < 0) || (start === null)||(end === null)||(step === null))
-  {  }
-  else{
-    for (let i = start;i <= end; i+=step ){
-     arr.push(i);  
-    }
+    const arr = [];
+    while(start <= end ){
+        if(step > 0 ){
+        arr.push(start);
+        start += step; 
+        }
+        else {
+        break;
+       }
+      }
+    return arr;
   }
-  return arr;
-  }
-    
-  console.log(range(3,0,2));
-  console.log(range(3,50,2));
+  
+     
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+console.log(range(3,0,1));
